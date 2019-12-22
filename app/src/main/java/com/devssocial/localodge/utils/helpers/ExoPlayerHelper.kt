8a44125ab61949fd0f11b9fa672fdd85309b67b6
Christilyn Arjona <christilynarjona@outlook.com>
2019-12-21 20:52:02 -0700

    updated exoplayer, implemented new api

--- app/src/main/java/com/devssocial/localodge/utils/helpers/ExoPlayerHelper.kt
+++ app/src/main/java/com/devssocial/localodge/utils/helpers/ExoPlayerHelper.kt
@@ -5,9 +5,6 @@ import com.devssocial.localodge.R
 import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
-import com.google.android.exoplayer2.source.TrackGroupArray
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
@@ -22,24 +19,13 @@ class ExoPlayerHelper(
     private var mediaSource: ProgressiveMediaSource? = null
 
     private val playerListener = object : Player.EventListener {
-        override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters?) {}
-        override fun onSeekProcessed() {}
-        override fun onTracksChanged(
-            trackGroups: TrackGroupArray?,
-            trackSelections: TrackSelectionArray?
-        ) {
+        override fun onPlayerError(error: ExoPlaybackException) {
+            super.onPlayerError(error)
+            onError(error)
         }
 
-        override fun onPlayerError(error: ExoPlaybackException?) {
-            error?.let { onError(it) }
-        }
-
-        override fun onLoadingChanged(isLoading: Boolean) {}
-        override fun onPositionDiscontinuity(reason: Int) {}
-        override fun onRepeatModeChanged(repeatMode: Int) {}
-        override fun onShuffleModeEnabledChanged(shuffleModeEnabled: Boolean) {}
-        override fun onTimelineChanged(timeline: Timeline?, manifest: Any?, reason: Int) {}
         override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+            super.onPlayerStateChanged(playWhenReady, playbackState)
             onPlayerBuffer(playbackState == Player.STATE_BUFFERING)
         }
     }
@@ -50,21 +36,11 @@ class ExoPlayerHelper(
     }
 
     fun initializePlayer(url: String) {
-        val trackSelector = DefaultTrackSelector()
-        val loadControl = DefaultLoadControl()
-        val renderersFactory = DefaultRenderersFactory(playerView.context)
-
-        exoPlayer = ExoPlayerFactory.newSimpleInstance(
-            playerView.context,
-            renderersFactory,
-            trackSelector,
-            loadControl
-        )
+        exoPlayer = SimpleExoPlayer.Builder(playerView.context).build()
         exoPlayer!!.repeatMode = Player.REPEAT_MODE_ALL
         exoPlayer!!.addListener(playerListener)
 
         playerView.player = exoPlayer
-//        exoPlayer!!.seekTo(currentWindow, playbackPosition)
 
         val userAgent =
             Util.getUserAgent(playerView.context, playerView.context.getString(R.string.app_name))
@@ -75,7 +51,7 @@ class ExoPlayerHelper(
             )
             .createMediaSource(Uri.parse(url))
 
-        exoPlayer!!.prepare(mediaSource, true, false)
+        exoPlayer!!.prepare(mediaSource!!, true, false)
         exoPlayer!!.playWhenReady = true
     }
 
